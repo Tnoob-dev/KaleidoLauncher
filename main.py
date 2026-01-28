6eaae141455c89904ae55607866160a01a191be5
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2026-01-28 18:14:15 -0500

    testing compilation, switched styles to main.py

--- main.py
+++ main.py
@@ -9,7 +9,95 @@ import asyncio
 class MyApp(App):
     ENABLE_COMMAND_PALETTE = False
     
-    CSS_PATH = "./app/src/styles/ProfileScreen.tcss"
+    CSS = """
+    /* Profile doesnt exists Screen */
+
+Profiles {
+    align: center middle;
+    color: #ffffff;
+    layout: vertical;
+    text-style: none;
+}
+
+Button {
+    text-style: none;
+}
+
+Profiles > Label {
+    background: #000000;
+    align: center middle;
+}
+
+#no_config_label {
+    content-align-horizontal: center;
+    text-style: reverse;
+}
+
+#create_profile_btn {
+    margin: 2 0 0 6;
+    height: auto;
+    background: transparent;
+    color: #EAEFEF;
+    text-style: none;
+    border: round #EAEFEF;
+}
+
+/* Profile Creation Screen */
+
+ProfileCreation {
+    align: center middle;
+    padding: 0 -5;
+}
+
+#create_btn {
+    margin: 0 0 0 50;
+    height: auto;
+    background: transparent;
+    color: #EAEFEF;
+    text-style: none;
+    border: round #EAEFEF;
+}
+
+#name_input {
+    width: 30%;
+}
+
+#version_select {
+    width: auto;
+}
+
+.error_label {
+    color: #BF616A;
+    text-style: bold;
+    text-align: center;
+    margin: 15 0 0 37;
+}
+
+.hidden {
+    display: none;
+}
+
+#retry_connection_btn {
+    margin: 1 0 0 50;
+    height: auto;
+    background: transparent;
+    color: #EAEFEF;
+    text-style: none;
+    border: round #EAEFEF;
+}
+
+.form-row {
+    margin: 1 0 0 25;
+}
+
+#name_label {
+    margin: 1 0 0 0;
+}
+
+#version_label {
+    margin: 1 0 0 3;
+}
+    """
     
     BINDINGS = [("q, ctrl+c", "quit", "Cerrar"),
                 ("1", "change_theme('minecraft')", "Tema de Minecraft"),
@@ -35,7 +123,7 @@ class MyApp(App):
         await asyncio.to_thread(createKaleidoFolder, platformPath)
         self.push_screen(Profiles())
         
-    def action_change_theme(self, themeName: str) -> None:        
+    def action_change_theme(self, themeName: str) -> None:
         self.theme = themeName
         
 if __name__ == "__main__":
